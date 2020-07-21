f8d96e72b2d3419f4736360eb0b969cdce21f91d
Tomáš Trávníček <travnicek.t494@gmail.com>
2020-07-21 12:15:02 +0200

    added redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,28 @@
 import React from "react";
+import axios from "axios";
 import Navbar from "./Navbar";
 import MainContainer from "./Layout/MainContainer";
+import UserSearch from "./UserSearch";
 
 const UserRepos = (props) => {
   React.useEffect(() => {
     console.log("XS");
-    let url = "https://api.github.com/users/gaearon";
-    fetch(url)
-      .then((res) => res.json())
-      .then((json) => console.log(json));
+    let user = "thomas4t"; //gaearon
+    let url = `https://api.github.com/users/${user}`;
+    // fetch(url)
+    //   .then((res) => res.json())
+    //   .then((json) => console.log(json));
+    axios
+      .get(`https://api.github.com/search/users?q=thomas4t`)
+      .then((res) => console.log(res.data));
   }, []);
   return (
     <MainContainer>
       <header>
         <Navbar />
       </header>
-      <p>React app aka Home.</p>
+      <p>Find a user</p>
+      <UserSearch />
       <p className="colorTest">Test of color</p>
     </MainContainer>
   );
---
+++ src/components/UserSearch.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserSearch = (props) => {
+  return <input type="text"></input>;
+};
+
+export default UserSearch;
