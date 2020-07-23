7ab9d443a0132b1b78417e43eb1b3238f56e4b73
Tomáš Trávníček <travnicek.t494@gmail.com>
2020-07-23 17:42:58 +0200

    router changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import UserRepos from "./components/RepositoriesPage";
 import Home from "./components/HomePage";
