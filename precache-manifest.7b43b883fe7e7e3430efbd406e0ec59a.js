e8a4ec562db661918eacc573ebd174b17c158654
Tomáš Trávníček <travnicek.t494@gmail.com>
2020-07-23 17:17:28 +0200

    Updates

---
+++ precache-manifest.7b43b883fe7e7e3430efbd406e0ec59a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9515e97ac2589f492a0665c92ac3dc",
+    "url": "/cleoFinanceTest/index.html"
+  },
+  {
+    "revision": "2ab6f7c5fbe0d441144f",
+    "url": "/cleoFinanceTest/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "0aa59eb823c68ab48407",
+    "url": "/cleoFinanceTest/static/js/2.a9874c5d.chunk.js"
+  },
+  {
+    "revision": "966a00a9d0da866151d459b5ca048ede",
+    "url": "/cleoFinanceTest/static/js/2.a9874c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab6f7c5fbe0d441144f",
+    "url": "/cleoFinanceTest/static/js/main.300222a6.chunk.js"
+  },
+  {
+    "revision": "d745b83a524d4e06ec69",
+    "url": "/cleoFinanceTest/static/js/runtime-main.3e58ec32.js"
+  }
+]);
